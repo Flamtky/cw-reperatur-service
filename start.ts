0e2f9c5452e01b8945e3d6b677575eabee982f4a
Flamtky <n.schumann@rz-online.de>
2021-11-24 00:38:46 +0100

    fixed start.ts

--- start.ts
+++ start.ts
@@ -1,11 +1,11 @@
 import * as WebServer from "./util/WebServer";
 import HttpFileHandler from "./util/HttpFileHandler";
 import HttpRateLimiter from "./util/HttpRateLimiter";
-import AuthHandler from "./api/authHandler";
+import apiHandler from "./api/apiHandler";
 
 function startWebServer() {
     WebServer.addHttpListener(new HttpRateLimiter(200, 5*60*1000, "/"));
-    WebServer.addHttpPathHandler(new HttpFileHandler("web", "/api", new AuthHandler()));
+    WebServer.addHttpPathHandler(new HttpFileHandler("web", "/api", undefined, new apiHandler()));
     WebServer.addHttpPathHandler(new HttpFileHandler("web", "/"));
     WebServer.start();
 }
